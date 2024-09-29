3c13d1be794f3d36d3af2b2677ce1d4f103369ce
CharlotteNoraJones <charlottenorajones@gmail.com>
2024-09-29 13:26:16 -0500

    Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,72 @@
+#include "stdio.h"
+
+// glad must come before GLFW
+#include "glad.h"
+#include "GLFW/glfw3.h"
+
+// this specific function signature is required for use as a GLFW error handling callback
+void error_callback(int error, const char *description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main()
+{
+    // glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+    {
+        fprintf(stderr, "GLFW failed to initialize");
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow *window = glfwCreateWindow(640, 480, "GLFW Window", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        glfwTerminate();
+        fprintf(stderr, "GLFW window failed to initialize");
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        fprintf(stderr, "GLAD failed to initialize");
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
